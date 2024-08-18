609ad04f14affa680da1c865b135df4eee6d298a
TonyLin <chiacheng.lin0@gmail.com>
2024-08-19 01:24:41 +0800

    update

--- src/main.py
+++ src/main.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
+    import nltk
+    nltk.download('punkt')
     load_dotenv(override=True)
     from streamlit_ui import check_idle, main
 
